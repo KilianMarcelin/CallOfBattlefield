9dc16413e65c35c87ae6b60f510981e50fc58d2f
Théo Le Goc <theo@legoc.net>
2024-02-12 18:22:46 +0100

    Fixed second player not moving

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,51 +1,92 @@
 ﻿using Mirror;
 using UnityEngine;
 
-namespace QuickStart
+public class PlayerScript : NetworkBehaviour
 {
-    public class PlayerScript : NetworkBehaviour
+    public Rigidbody rb;
+
+    public float jumpForce = .3f;
+
+    [SyncVar]
+    public float health = 100f;
+
+    private bool hasJumped = false;
+
+    [Server]
+    public void ServerHit(float damage)
     {
-        public Rigidbody rb;
-        
-        public float jumpForce = 5f;
-        
-        public float health = 100f;
+        health -= damage;
+        Debug.Log("Took damage, health: " + health);
 
-        public void Hit(float damage)
+        if (health <= 0)
         {
             
         }
+    }
+
+    public override void OnStartAuthority()
+    {
+        Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = new Vector3(0, 1.8f, 0);
+
+        name = "LocalPlayer" + Random.Range(1, 1000);
         
-        public override void OnStartLocalPlayer()
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.02f);
+    }
+
+    void Update()
+    {
+        if (!isOwned)
         {
-            Camera.main.transform.SetParent(transform);
-            Camera.main.transform.localPosition = new Vector3(0, 1.8f, 0);
-            
-            // Lock cursor
-            Cursor.lockState = CursorLockMode.Locked;
+            return;
         }
 
-        void Update()
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 4f;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, moveX, 0);
+        transform.Translate(moveX, 0, moveZ);
+
+        Camera.main.transform.Rotate(-mouseY, 0, 0);
+        transform.Rotate(0, mouseX, 0);
+
+        if (Input.GetButton("Jump") && !hasJumped)
         {
-            if (!isLocalPlayer) { return; }
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJumped = true;
+        } else if (IsGrounded())
+        {
+            hasJumped = false;
+        }
+        
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Shooting");
+            Vector3 origin = Camera.main.transform.position;
+            Vector3 direction = Camera.main.transform.forward;
+            CmdShoot(origin, direction);
+        }
+    }
 
-            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 4f;
-            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
-
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
-
-            transform.Rotate(0, moveX, 0);
-            transform.Translate(moveX, 0, moveZ);
-            
-            // Rotate with clamping
-            Camera.main.transform.Rotate(-mouseY, 0, 0);
-            transform.Rotate(0, mouseX, 0);
-            
-            // Clamp camera rotation
-            if (Input.GetButton("Jump"))
+    [Command]
+    public void CmdShoot(Vector3 origin, Vector3 direction)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(origin, direction, out hit, 100f))
+        {
+            PlayerScript player = hit.transform.GetComponent<PlayerScript>();
+            if (player != null)
             {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                player.ServerHit(10f);
             }
         }
     }
