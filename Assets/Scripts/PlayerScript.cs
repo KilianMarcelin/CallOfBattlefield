5229c30ada781e6c9604c68e76bba1d72ae0ea70
Théo Le Goc <theo@legoc.net>
2024-02-13 00:21:24 +0100

    Animations!!!

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,11 +7,13 @@ public class PlayerScript : NetworkBehaviour
     public float speed = 4f;
     public float jumpForce = .3f;
     public Animator animator;
-    
-    [SyncVar]
-    public float health = 100f;
+    public GameObject sourceObject;
+
+    [SyncVar] public float health = 100f;
 
     private bool hasJumped = false;
+
+    private float verticalAngle = 0.0f;
 
     [Server]
     public void ServerHit(float damage)
@@ -21,17 +23,20 @@ public class PlayerScript : NetworkBehaviour
 
         if (health <= 0)
         {
-            
         }
     }
 
     public override void OnStartAuthority()
     {
         Camera.main.transform.SetParent(transform);
-        Camera.main.transform.localPosition = new Vector3(0, 1.8f, 0);
+        Camera.main.transform.localPosition = new Vector3(0, 1.7f, 0);
+
+        // aim
+        sourceObject.transform.SetParent(Camera.main.transform);
+        sourceObject.transform.localPosition = new Vector3(0, 0, 20);
 
         name = "LocalPlayer" + Random.Range(1, 1000);
-        
+
         // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -50,13 +55,14 @@ public class PlayerScript : NetworkBehaviour
 
         float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float moveZ = Input.GetAxis("Vertical");
-        
+
         if (moveZ < 0)
         {
             moveZ *= .5f;
         }
-        moveZ *= Time.deltaTime *speed;
-        
+
+        moveZ *= Time.deltaTime * speed;
+
         animator.SetFloat("forward", Input.GetAxis("Vertical"));
         animator.SetFloat("left", -Input.GetAxis("Horizontal"));
 
@@ -66,18 +72,26 @@ public class PlayerScript : NetworkBehaviour
         transform.Rotate(0, moveX, 0);
         transform.Translate(moveX, 0, moveZ);
 
-        Camera.main.transform.Rotate(-mouseY, 0, 0);
+
+        verticalAngle += mouseY;
+        if (verticalAngle < -90f) verticalAngle = -90f;
+        else if (verticalAngle > 90f) verticalAngle = 90f;
+
+        animator.SetFloat("aim", verticalAngle / 90f);
+
+        Camera.main.transform.localRotation = Quaternion.AngleAxis(verticalAngle, Vector3.left);
         transform.Rotate(0, mouseX, 0);
 
         if (Input.GetButton("Jump") && !hasJumped)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             hasJumped = true;
-        } else if (IsGrounded())
+        }
+        else if (IsGrounded())
         {
             hasJumped = false;
         }
-        
+
         if (Input.GetButton("Fire1"))
         {
             Debug.Log("Shooting");
