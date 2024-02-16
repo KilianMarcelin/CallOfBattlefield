52c054c10e09dd4facd9b3d4d5c501e275d6f04f
Théo Le Goc <theo@legoc.net>
2024-02-16 12:56:13 +0100

    Die almost working, adding fx, animations

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,40 +9,126 @@ public class PlayerScript : NetworkBehaviour
     public float speed = 4f;
     public float jumpForce = .3f;
     public Animator animator;
-    public GameObject sourceObject;
+    public GameObject arms;
+    public GameObject body;
+    public GameObject gunSlot;
     public SphereCollider groundCollider;
+    public WeaponBehaviour weaponBehaviour;
+    public float respawnTime = 5;
 
+    public int currentWeapon = 0;
+    public Weapon[] weapons;
+        
+    [SyncVar]
+    private float timeUntilShoot = 0;
+    
+    [SyncVar] public bool isDed = false;
     [SyncVar] public float health = 100f;
 
     private bool hasJumped = false;
     private bool isInAir = false;
 
     private float verticalAngle = 0.0f;
+    
+    [SyncVar]
+    private float timeUntilRespawn = 0;
 
+    public override void OnStartAuthority()
+    {
+        ClientSpawn();
+        CmdCallServerSpawn();
+    }
+    
+    [Command]
+    public void CmdCallServerHit(float damage)
+    {
+        ServerHit(damage);
+    }
+    
     [Server]
     public void ServerHit(float damage)
     {
         health -= damage;
         Debug.Log("Took damage, health: " + health);
-
+        
         if (health <= 0)
         {
+            ServerDie();
+            RpcCallClientDie();
+        }
+    }
+    
+
+    [Client]
+    public void ClientDie()
+    {
+        if (isOwned)
+        {
+            arms.SetActive(false);
+        }
+        else
+        {
+            body.SetActive(false);
         }
     }
 
-    public override void OnStartAuthority()
+    [Server]
+    public void ServerDie()
     {
-        Camera.main.transform.SetParent(transform);
-        Camera.main.transform.localPosition = new Vector3(0, 1.7f, 0);
+        isDed = true;
+        timeUntilRespawn = respawnTime;
+    }
+    
+    [ClientRpc]
+    public void RpcCallClientDie()
+    {
+        ClientDie();
+    }
 
-        // aim
-        sourceObject.transform.SetParent(Camera.main.transform);
-        sourceObject.transform.localPosition = new Vector3(0, 0, 20);
+    [Client]
+    public void ClientSpawn()
+    {
+        if (isOwned)
+        {
+            animator.gameObject.SetActive(false);
+            arms.SetActive(true);
 
-        name = "LocalPlayer" + Random.Range(1, 1000);
+            Camera.main.transform.SetParent(transform);
+            Camera.main.transform.localPosition = new Vector3(0, 1.7f, 0);
+            arms.transform.SetParent(Camera.main.transform);
+            arms.transform.localPosition = new Vector3(0, -1.65f, 0.11f);
 
-        // Lock cursor
-        Cursor.lockState = CursorLockMode.Locked;
+            name = "LocalPlayer" + Random.Range(1, 1000);
+
+            // Lock cursor
+            Cursor.lockState = CursorLockMode.Locked;
+
+            ChangeWeapon(0);
+        }
+        else
+        {
+            body.SetActive(true);
+        }
+    }
+    
+    [Server]
+    public void ServerSpawn()
+    {
+        isDed = false;
+        transform.position = NetworkManager.singleton.GetStartPosition().position;
+        health = 100f;
+    }
+
+    [ClientRpc]
+    public void RpcCallClientSpawn()
+    {
+        ClientSpawn();
+    }
+    
+    [Command]
+    public void CmdCallServerSpawn()
+    {
+        ServerSpawn();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -55,59 +141,91 @@ public class PlayerScript : NetworkBehaviour
         isInAir = true;
     }
 
+    public void ChangeWeapon(int index)
+    {
+        if (index < 0 || index >= weapons.Length) return;
+        currentWeapon = index;
+        // Remove all childs
+        foreach (Transform child in gunSlot.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        // Add new weapon
+        GameObject weapon = Instantiate(weapons[currentWeapon].weaponModel, gunSlot.transform);
+        weaponBehaviour = weapon.AddComponent<WeaponBehaviour>();
+    }
+    
     void Update()
     {
-        if (!isOwned)
+        if (isOwned && !isDed)
         {
-            return;
-        }
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
 
-        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float moveZ = Input.GetAxis("Vertical");
+            moveX *= Time.deltaTime * speed;
+            moveZ *= Time.deltaTime * speed;
 
-        if (moveZ < 0)
-        {
-            moveZ *= .5f;
-        }
+            animator.SetFloat("forward", Input.GetAxis("Vertical"));
+            animator.SetFloat("left", -Input.GetAxis("Horizontal"));
 
-        moveZ *= Time.deltaTime * speed;
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
 
-        animator.SetFloat("forward", Input.GetAxis("Vertical"));
-        animator.SetFloat("left", -Input.GetAxis("Horizontal"));
-
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        transform.Rotate(0, moveX, 0);
-        transform.Translate(moveX, 0, moveZ);
+            transform.Rotate(0, moveX, 0);
+            transform.Translate(moveX, 0, moveZ);
 
 
-        verticalAngle += mouseY;
-        if (verticalAngle < -90f) verticalAngle = -90f;
-        else if (verticalAngle > 90f) verticalAngle = 90f;
+            verticalAngle += mouseY;
+            if (verticalAngle < -90f) verticalAngle = -90f;
+            else if (verticalAngle > 90f) verticalAngle = 90f;
 
-        animator.SetFloat("aim", verticalAngle / 90f);
+            animator.SetFloat("aim", verticalAngle / 90f);
 
-        Camera.main.transform.localRotation = Quaternion.AngleAxis(verticalAngle, Vector3.left);
-        transform.Rotate(0, mouseX, 0);
+            Camera.main.transform.localRotation = Quaternion.AngleAxis(verticalAngle, Vector3.left);
+            transform.Rotate(0, mouseX, 0);
         
-        if (!isInAir && Input.GetButtonDown("Jump"))
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            if (!isInAir && Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            }
+
+            if (timeUntilShoot <= 0 && Input.GetButton("Fire1"))
+            {
+                Vector3 origin = Camera.main.transform.position;
+                Vector3 direction = Camera.main.transform.forward;
+                CmdShoot(origin, direction);
+            }
+
+            if (Input.GetButton("Power word kill"))
+            {
+                Debug.Log("You should kill yourself, NOW!");
+                CmdCallServerHit(1000f);
+            }
         }
 
-        if (Input.GetButton("Fire1"))
+        if (isServer)
         {
-            Debug.Log("Shooting");
-            Vector3 origin = Camera.main.transform.position;
-            Vector3 direction = Camera.main.transform.forward;
-            CmdShoot(origin, direction);
+            if (timeUntilShoot > 0) timeUntilShoot -= Time.deltaTime;
+            
+            // Respawning
+            if (isDed && timeUntilRespawn > 0)
+            {
+                timeUntilRespawn -= Time.deltaTime;
+                if (timeUntilRespawn <= 0)
+                {
+                    ServerSpawn();
+                    RpcCallClientSpawn();
+                }
+            }
         }
     }
 
     [Command]
     public void CmdShoot(Vector3 origin, Vector3 direction)
     {
+        if (timeUntilShoot > 0) return;
+        
+        timeUntilShoot = 1/weapons[currentWeapon].fireRate;
         RaycastHit hit;
         if (Physics.Raycast(origin, direction, out hit, 100f))
         {
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "weapon", menuName = "ScriptableObjects/Weapon", order = 1)]
+public class Weapon : ScriptableObject
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+    public GameObject weaponModel;
+}
