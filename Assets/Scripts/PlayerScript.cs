ae807b0521f7969445eabd5e8dc469214666f841
Théo Le Goc <theo@legoc.net>
2024-02-19 14:10:59 +0100

    Fixed animations, sway, recoil, breathing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,56 +9,68 @@ public class PlayerScript : NetworkBehaviour
     public float speed = 4f;
     public float jumpForce = .3f;
     public Animator animator;
+    public SphereCollider groundCollider;
+
     public GameObject arms;
     public GameObject body;
+
+    public Camera mainCamera;
+    public Camera armsCamera;
+    public String armsLayer;
+    public String hiddenLayer;
     public GameObject gunSlotFP;
     public GameObject gunSlotTP;
-    public SphereCollider groundCollider;
     public WeaponBehaviour weaponBehaviour;
     public ParticleSystem shootFX;
+    public float swayOffsetRate = 0.1f;
+    public float swayStrength = 0.1f;
+    public float breathSpeed = 1.0f;
+    public float breathStrength = 0.1f;
+    public float recoilAmount = 0.1f;
+    private float recoil = 0.0f;
+    private Vector2 swayOffset = Vector2.zero;
+    private Vector3 originalAmrsPos;
+
     public float respawnTime = 5;
+    [SyncVar] public bool isDed = false;
+    [SyncVar] public float health = 100f;
 
     [SyncVar] public int currentWeapon = 0;
     public Weapon[] weapons;
-        
-    [SyncVar]
-    private float timeUntilShoot = 0;
-    
-    [SyncVar] public bool isDed = false;
-    [SyncVar] public float health = 100f;
+
+    [SyncVar] private float timeUntilShoot = 0;
 
     private bool isInAir = false;
 
     private float verticalAngle = 0.0f;
-    
-    [SyncVar]
-    private float timeUntilRespawn = 0;
+
+    [SyncVar] private float timeUntilRespawn = 0;
 
     public override void OnStartAuthority()
     {
         ClientSpawn();
         CmdCallServerSpawn();
     }
-    
+
     [Command]
     public void CmdCallServerHit(float damage)
     {
         ServerHit(damage);
     }
-    
+
     [Server]
     public void ServerHit(float damage)
     {
         health -= damage;
         Debug.Log("Took damage, health: " + health);
-        
+
         if (health <= 0)
         {
             ServerDie();
             RpcCallClientDie();
         }
     }
-    
+
 
     [Client]
     public void ClientDie()
@@ -79,7 +91,7 @@ public class PlayerScript : NetworkBehaviour
         isDed = true;
         timeUntilRespawn = respawnTime;
     }
-    
+
     [ClientRpc]
     public void RpcCallClientDie()
     {
@@ -92,13 +104,19 @@ public class PlayerScript : NetworkBehaviour
         shootFX.transform.SetParent(null);
         if (isOwned)
         {
-            animator.gameObject.SetActive(false);
             arms.SetActive(true);
+            
+            // Set main camera
+            mainCamera.gameObject.tag = "MainCamera";
+            mainCamera.gameObject.SetActive(true);
 
-            Camera.main.transform.SetParent(transform);
-            Camera.main.transform.localPosition = new Vector3(0, 1.5f, 0);
-            arms.transform.SetParent(Camera.main.transform);
-            arms.transform.localPosition = new Vector3(0, -1.65f, 0.11f);
+            // Camera.main.transform.SetParent(transform);
+            // Camera.main.transform.localPosition = new Vector3(0, 1.5f, 0);
+            // Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("ArmsLayer"));
+            // armsCamera.transform.SetParent(Camera.main.transform);
+            // armsCamera.transform.localPosition = Vector3.zero;
+            // arms.transform.SetParent(Camera.main.transform);
+            // arms.transform.localPosition = new Vector3(0, -1.65f, 0.11f);
 
             name = "LocalPlayer" + Random.Range(1, 10000);
 
@@ -106,7 +124,7 @@ public class PlayerScript : NetworkBehaviour
             Cursor.lockState = CursorLockMode.Locked;
 
             CmdCallChangeWeapon(0);
-            
+
             PlayerScript[] players = FindObjectsOfType<PlayerScript>();
             for (int i = 0; i < players.Length; i++)
             {
@@ -115,6 +133,12 @@ public class PlayerScript : NetworkBehaviour
                     players[i].ClientChangeWeapon(players[i].currentWeapon);
                 }
             }
+            
+            SetGameLayerRecursive(shootFX.gameObject, LayerMask.NameToLayer("ArmsLayer"));
+
+            originalAmrsPos = arms.transform.localPosition;
+            
+            SetGameLayerRecursive(body.gameObject, LayerMask.NameToLayer(hiddenLayer));
         }
         else
         {
@@ -135,7 +159,7 @@ public class PlayerScript : NetworkBehaviour
     {
         ClientSpawn();
     }
-    
+
     [Command]
     public void CmdCallServerSpawn()
     {
@@ -157,46 +181,65 @@ public class PlayerScript : NetworkBehaviour
     {
         ServerChangeWeapon(index);
     }
-    
+
     [Server]
     public void ServerChangeWeapon(int index)
     {
         currentWeapon = index;
         RpcCallClientChangeWeapon(index);
     }
-    
+
     [ClientRpc]
     public void RpcCallClientChangeWeapon(int index)
     {
         ClientChangeWeapon(index);
     }
+
+    private void SetGameLayerRecursive(GameObject _go, int _layer)
+    {
+        _go.layer = _layer;
+        foreach (Transform child in _go.transform)
+        {
+            child.gameObject.layer = _layer;
+     
+            Transform _HasChildren = child.GetComponentInChildren<Transform>();
+            if (_HasChildren != null)
+                SetGameLayerRecursive(child.gameObject, _layer);
+                 
+        }
+    }
+
     
     [Client]
     public void ClientChangeWeapon(int index)
     {
-        
         Debug.Log("Changed weapon to " + index);
         // Remove all childs
         foreach (Transform child in gunSlotFP.transform)
         {
             Destroy(child.gameObject);
         }
+
         foreach (Transform child in gunSlotTP.transform)
         {
             Destroy(child.gameObject);
         }
-        
+
         // Add new weapon
         GameObject weapon = Instantiate(weapons[index].weaponModel);
         if (!isOwned) weapon.transform.SetParent(gunSlotTP.transform);
-        else weapon.transform.SetParent(gunSlotFP.transform);
-        
+        else
+        {
+            weapon.transform.SetParent(gunSlotFP.transform);
+            SetGameLayerRecursive(weapon, LayerMask.NameToLayer("ArmsLayer"));
+        }
+
         weapon.transform.localPosition = Vector3.zero;
         weapon.transform.localEulerAngles = Vector3.zero;
-        
+
         weaponBehaviour = weapon.GetComponent<WeaponBehaviour>();
     }
-    
+
     void Update()
     {
         if (isOwned && !isDed)
@@ -213,6 +256,13 @@ public class PlayerScript : NetworkBehaviour
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
 
+            // Procedural arms sway and other stuff
+            swayOffset.x = Mathf.Lerp(swayOffset.x, mouseX, swayOffsetRate);
+            swayOffset.y = Mathf.Lerp(swayOffset.y, mouseY, swayOffsetRate);
+            
+            recoil = Mathf.Lerp(recoil, 0, 0.1f);
+            arms.transform.localPosition = originalAmrsPos + new Vector3(swayOffset.x, swayOffset.y + Mathf.Sin(Time.time * breathSpeed) * breathStrength, -recoil) * swayStrength;
+
             transform.Rotate(0, moveX, 0);
             transform.Translate(moveX, 0, moveZ);
 
@@ -223,9 +273,9 @@ public class PlayerScript : NetworkBehaviour
 
             animator.SetFloat("aim", verticalAngle / 90f);
 
-            Camera.main.transform.localRotation = Quaternion.AngleAxis(verticalAngle, Vector3.left);
+            mainCamera.transform.localRotation = Quaternion.AngleAxis(verticalAngle, Vector3.left);
             transform.Rotate(0, mouseX, 0);
-        
+
             if (!isInAir && Input.GetButtonDown("Jump"))
             {
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
@@ -233,9 +283,10 @@ public class PlayerScript : NetworkBehaviour
 
             if (timeUntilShoot <= 0 && Input.GetButton("Fire1"))
             {
-                Vector3 origin = Camera.main.transform.position;
-                Vector3 direction = Camera.main.transform.forward;
+                Vector3 origin = mainCamera.transform.position;
+                Vector3 direction = mainCamera.transform.forward;
                 CmdShoot(origin, direction);
+                recoil += recoilAmount;
             }
 
             if (Input.GetButton("Power word kill"))
@@ -248,7 +299,7 @@ public class PlayerScript : NetworkBehaviour
         if (isServer)
         {
             if (timeUntilShoot > 0) timeUntilShoot -= Time.deltaTime;
-            
+
             // Respawning
             if (isDed && timeUntilRespawn > 0)
             {
@@ -268,8 +319,8 @@ public class PlayerScript : NetworkBehaviour
         if (timeUntilShoot > 0) return;
 
         RpcPlayShootAnim();
-        
-        timeUntilShoot = 1/weapons[currentWeapon].fireRate;
+
+        timeUntilShoot = 1 / weapons[currentWeapon].fireRate;
         RaycastHit hit;
         if (Physics.Raycast(origin, direction, out hit, 100f))
         {
@@ -280,7 +331,7 @@ public class PlayerScript : NetworkBehaviour
             }
         }
     }
-    
+
     [ClientRpc]
     public void RpcPlayShootAnim()
     {
