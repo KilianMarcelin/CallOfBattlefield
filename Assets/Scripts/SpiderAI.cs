63ce8129275d5d75ebbcc58059bba86f8836de18
kmarcelin <kilian.marcelin@etudiant.univ-rennes1.fr>
2024-03-15 11:13:36 +0100

    Spider

---
+++ Assets/Scripts/SpiderAI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderAI : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public NavMeshAgent agent;
+    public Animator animator;
+    private bool inAttackRange = false;
+    [SerializeField] private float attackRange = 3f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform)
+        {
+            if (Vector3.Distance(playerTransform.position, transform.position) > attackRange)
+            {
+                agent.SetDestination(playerTransform.position);
+                animator.SetBool("walking", true);
+                animator.SetBool("attack", false);
+            }
+            else
+            {
+                animator.SetBool("walking", false);
+                animator.SetBool("attack", true);
+            }
+
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+            animator.SetBool("attack", false);
+        }
+    }
+}
