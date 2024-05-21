9bb01faa53172d20e135c0ab2f6be20325162d3b
Théo Le Goc <theo@legoc.net>
2024-05-21 21:02:49 +0200

    Disable aura and post process when on low, better starts on forest

--- Assets/Scripts/UI/Options.cs
+++ Assets/Scripts/UI/Options.cs
@@ -1,26 +1,41 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Aura2API;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Options : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropdownQuality;
     [SerializeField] private GameObject toActive;
-
+    [SerializeField] private AuraVolume auraVolume;
+    [SerializeField] private PostProcessVolume ppVolume;
+    
     // Hacky fix
     public static bool isPaused = false;
 
     public void SetQuality(int i)
     {
         QualitySettings.SetQualityLevel(i, true);
+
+        if (i == 0) // Low
+        {
+            if (auraVolume) auraVolume.enabled = false;
+            if (ppVolume) ppVolume.enabled = false;
+        }
+        else
+        {
+            if (auraVolume) auraVolume.enabled = true;
+            if (ppVolume) ppVolume.enabled = true;
+        }
     }
 
     private void Start()
     {
         int qualityLevel = QualitySettings.GetQualityLevel();
         dropdownQuality.value = qualityLevel;
+        
+        // Update aura and pp volume
+        SetQuality(qualityLevel);
     }
 
     public void Pause()
